d2a8af351666f71b90e64ac784dc3452aca8cd40
Palash Hawee <haweepalash@gmail.com>
2024-09-16 15:15:45 +0600

    added frontend set up

---
+++ frontend/src/components/shared/Logo.tsx
@@ -0,0 +1,27 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+export default function Logo  () {
+  return (
+    <div style={{
+        display:'flex',
+         margin:'auto',
+         alignItems:"center",
+         gap:"8px"}} >
+
+    <Link to={'/'}>
+    <img 
+    src="/frontend/public/openai.png" 
+    alt="openai" 
+    width={"30px"}
+     height={"30px"} 
+     className='image-inverted'/>
+     <Typography sx={{display:{md:"block",sm:"none", xs:"none"}, mr:"auto", fontWeight:"800",textShadow:"2px 2px 20px #000"}}>
+        <span style={{fontSize:"20px"}}>MERN</span>-GPT
+     </Typography>
+    </Link>
+    </div>
+  )
+}
